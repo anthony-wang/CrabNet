a7d0afea4335d67e15a034a2e70cd44ea9656e99
sgbaird <sterling.baird@icloud.com>
2021-09-20 01:46:08 -0600

    black reformatting

--- crabnet/kingcrab.py
+++ crabnet/kingcrab.py
@@ -28,36 +28,39 @@ class ResidualNetwork(nn.Module):
         hidden_layer_dims: list(int)
         """
         super(ResidualNetwork, self).__init__()
-        dims = [input_dim]+hidden_layer_dims
-        self.fcs = nn.ModuleList([nn.Linear(dims[i], dims[i+1])
-                                  for i in range(len(dims)-1)])
-        self.res_fcs = nn.ModuleList([nn.Linear(dims[i], dims[i+1], bias=False)
-                                      if (dims[i] != dims[i+1])
-                                      else nn.Identity()
-                                      for i in range(len(dims)-1)])
-        self.acts = nn.ModuleList([nn.LeakyReLU() for _ in range(len(dims)-1)])
+        dims = [input_dim] + hidden_layer_dims
+        self.fcs = nn.ModuleList(
+            [nn.Linear(dims[i], dims[i + 1]) for i in range(len(dims) - 1)]
+        )
+        self.res_fcs = nn.ModuleList(
+            [
+                nn.Linear(dims[i], dims[i + 1], bias=False)
+                if (dims[i] != dims[i + 1])
+                else nn.Identity()
+                for i in range(len(dims) - 1)
+            ]
+        )
+        self.acts = nn.ModuleList([nn.LeakyReLU() for _ in range(len(dims) - 1)])
         self.fc_out = nn.Linear(dims[-1], output_dim)
 
     def forward(self, fea):
         for fc, res_fc, act in zip(self.fcs, self.res_fcs, self.acts):
-            fea = act(fc(fea))+res_fc(fea)
+            fea = act(fc(fea)) + res_fc(fea)
         return self.fc_out(fea)
 
     def __repr__(self):
-        return f'{self.__class__.__name__}'
+        return f"{self.__class__.__name__}"
 
 
 class Embedder(nn.Module):
-    def __init__(self,
-                 d_model,
-                 compute_device=None):
+    def __init__(self, d_model, compute_device=None):
         super().__init__()
         self.d_model = d_model
         self.compute_device = compute_device
 
-        elem_dir = 'data/element_properties'
+        elem_dir = "data/element_properties"
         # # Choose what element information the model receives
-        mat2vec = f'{elem_dir}/mat2vec.csv'  # element embedding
+        mat2vec = f"{elem_dir}/mat2vec.csv"  # element embedding
         # mat2vec = f'{elem_dir}/onehot.csv'  # onehot encoding (atomic number)
         # mat2vec = f'{elem_dir}/random_200.csv'  # random vec for elements
 
@@ -67,8 +70,9 @@ class Embedder(nn.Module):
         zeros = np.zeros((1, feat_size))
         cat_array = np.concatenate([zeros, cbfv])
         cat_array = torch.as_tensor(cat_array, dtype=data_type_torch)
-        self.cbfv = nn.Embedding.from_pretrained(cat_array) \
-            .to(self.compute_device, dtype=data_type_torch)
+        self.cbfv = nn.Embedding.from_pretrained(cat_array).to(
+            self.compute_device, dtype=data_type_torch
+        )
 
     def forward(self, src):
         mat2vec_emb = self.cbfv(src)
@@ -83,40 +87,35 @@ class FractionalEncoder(nn.Module):
     by the positional encoder discussed by Vaswani.
     https://arxiv.org/abs/1706.03762
     """
-    def __init__(self,
-                 d_model,
-                 resolution=100,
-                 log10=False,
-                 compute_device=None):
+
+    def __init__(self, d_model, resolution=100, log10=False, compute_device=None):
         super().__init__()
-        self.d_model = d_model//2
+        self.d_model = d_model // 2
         self.resolution = resolution
         self.log10 = log10
         self.compute_device = compute_device
 
-        x = torch.linspace(0, self.resolution - 1,
-                           self.resolution,
-                           requires_grad=False) \
-            .view(self.resolution, 1)
-        fraction = torch.linspace(0, self.d_model - 1,
-                                  self.d_model,
-                                  requires_grad=False) \
-            .view(1, self.d_model).repeat(self.resolution, 1)
+        x = torch.linspace(
+            0, self.resolution - 1, self.resolution, requires_grad=False
+        ).view(self.resolution, 1)
+        fraction = (
+            torch.linspace(0, self.d_model - 1, self.d_model, requires_grad=False)
+            .view(1, self.d_model)
+            .repeat(self.resolution, 1)
+        )
 
         pe = torch.zeros(self.resolution, self.d_model)
-        pe[:, 0::2] = torch.sin(x /torch.pow(
-            50,2 * fraction[:, 0::2] / self.d_model))
-        pe[:, 1::2] = torch.cos(x / torch.pow(
-            50, 2 * fraction[:, 1::2] / self.d_model))
-        pe = self.register_buffer('pe', pe)
+        pe[:, 0::2] = torch.sin(x / torch.pow(50, 2 * fraction[:, 0::2] / self.d_model))
+        pe[:, 1::2] = torch.cos(x / torch.pow(50, 2 * fraction[:, 1::2] / self.d_model))
+        pe = self.register_buffer("pe", pe)
 
     def forward(self, x):
         x = x.clone()
         if self.log10:
-            x = 0.0025 * (torch.log2(x))**2
+            x = 0.0025 * (torch.log2(x)) ** 2
             x[x > 1] = 1
             # x = 1 - x  # for sinusoidal encoding at x=0
-        x[x < 1/self.resolution] = 1/self.resolution
+        x[x < 1 / self.resolution] = 1 / self.resolution
         frac_idx = torch.round(x * (self.resolution)).to(dtype=torch.long) - 1
         out = self.pe[frac_idx]
 
@@ -125,13 +124,7 @@ class FractionalEncoder(nn.Module):
 
 # %%
 class Encoder(nn.Module):
-    def __init__(self,
-                 d_model,
-                 N,
-                 heads,
-                 frac=False,
-                 attn=True,
-                 compute_device=None):
+    def __init__(self, d_model, N, heads, frac=False, attn=True, compute_device=None):
         super().__init__()
         self.d_model = d_model
         self.N = N
@@ -139,25 +132,24 @@ class Encoder(nn.Module):
         self.fractional = frac
         self.attention = attn
         self.compute_device = compute_device
-        self.embed = Embedder(d_model=self.d_model,
-                              compute_device=self.compute_device)
+        self.embed = Embedder(d_model=self.d_model, compute_device=self.compute_device)
         self.pe = FractionalEncoder(self.d_model, resolution=5000, log10=False)
         self.ple = FractionalEncoder(self.d_model, resolution=5000, log10=True)
 
-        self.emb_scaler = nn.parameter.Parameter(torch.tensor([1.]))
-        self.pos_scaler = nn.parameter.Parameter(torch.tensor([1.]))
-        self.pos_scaler_log = nn.parameter.Parameter(torch.tensor([1.]))
+        self.emb_scaler = nn.parameter.Parameter(torch.tensor([1.0]))
+        self.pos_scaler = nn.parameter.Parameter(torch.tensor([1.0]))
+        self.pos_scaler_log = nn.parameter.Parameter(torch.tensor([1.0]))
 
         if self.attention:
-            encoder_layer = nn.TransformerEncoderLayer(self.d_model,
-                                                       nhead=self.heads,
-                                                       dim_feedforward=2048,
-                                                       dropout=0.1)
-            self.transformer_encoder = nn.TransformerEncoder(encoder_layer,
-                                                             num_layers=self.N)
+            encoder_layer = nn.TransformerEncoderLayer(
+                self.d_model, nhead=self.heads, dim_feedforward=2048, dropout=0.1
+            )
+            self.transformer_encoder = nn.TransformerEncoder(
+                encoder_layer, num_layers=self.N
+            )
 
     def forward(self, src, frac):
-        x = self.embed(src) * 2**self.emb_scaler
+        x = self.embed(src) * 2 ** self.emb_scaler
         mask = frac.unsqueeze(dim=-1)
         mask = torch.matmul(mask, mask.transpose(-2, -1))
         mask[mask != 0] = 1
@@ -165,16 +157,15 @@ class Encoder(nn.Module):
 
         pe = torch.zeros_like(x)
         ple = torch.zeros_like(x)
-        pe_scaler = 2**(1-self.pos_scaler)**2
-        ple_scaler = 2**(1-self.pos_scaler_log)**2
-        pe[:, :, :self.d_model//2] = self.pe(frac) * pe_scaler
-        ple[:, :, self.d_model//2:] = self.ple(frac) * ple_scaler
+        pe_scaler = 2 ** (1 - self.pos_scaler) ** 2
+        ple_scaler = 2 ** (1 - self.pos_scaler_log) ** 2
+        pe[:, :, : self.d_model // 2] = self.pe(frac) * pe_scaler
+        ple[:, :, self.d_model // 2 :] = self.ple(frac) * ple_scaler
 
         if self.attention:
             x_src = x + pe + ple
             x_src = x_src.transpose(0, 1)
-            x = self.transformer_encoder(x_src,
-                                         src_key_padding_mask=src_mask)
+            x = self.transformer_encoder(x_src, src_key_padding_mask=src_mask)
             x = x.transpose(0, 1)
 
         if self.fractional:
@@ -189,12 +180,7 @@ class Encoder(nn.Module):
 
 # %%
 class CrabNet(nn.Module):
-    def __init__(self,
-                 out_dims=3,
-                 d_model=512,
-                 N=3,
-                 heads=4,
-                 compute_device=None):
+    def __init__(self, out_dims=3, d_model=512, N=3, heads=4, compute_device=None):
         super().__init__()
         self.avg = True
         self.out_dims = out_dims
@@ -202,14 +188,14 @@ class CrabNet(nn.Module):
         self.N = N
         self.heads = heads
         self.compute_device = compute_device
-        self.encoder = Encoder(d_model=self.d_model,
-                               N=self.N,
-                               heads=self.heads,
-                               compute_device=self.compute_device)
+        self.encoder = Encoder(
+            d_model=self.d_model,
+            N=self.N,
+            heads=self.heads,
+            compute_device=self.compute_device,
+        )
         self.out_hidden = [1024, 512, 256, 128]
-        self.output_nn = ResidualNetwork(self.d_model,
-                                         self.out_dims,
-                                         self.out_hidden)
+        self.output_nn = ResidualNetwork(self.d_model, self.out_dims, self.out_hidden)
 
     def forward(self, src, frac):
         output = self.encoder(src, frac)
@@ -220,15 +206,15 @@ class CrabNet(nn.Module):
         output = self.output_nn(output)  # simple linear
         if self.avg:
             output = output.masked_fill(mask, 0)
-            output = output.sum(dim=1)/(~mask).sum(dim=1)
+            output = output.sum(dim=1) / (~mask).sum(dim=1)
             output, logits = output.chunk(2, dim=-1)
             probability = torch.ones_like(output)
-            probability[:, :logits.shape[-1]] = torch.sigmoid(logits)
+            probability[:, : logits.shape[-1]] = torch.sigmoid(logits)
             output = output * probability
 
         return output
 
 
 # %%
-if __name__ == '__main__':
+if __name__ == "__main__":
     model = CrabNet()
