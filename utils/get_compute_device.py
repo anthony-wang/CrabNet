c89342e804cd18f855d7107b7d20b21946e29cf0
sgbaird <sterling.baird@icloud.com>
2021-09-20 01:47:00 -0600

    black reformatting

--- utils/get_compute_device.py
+++ utils/get_compute_device.py
@@ -7,15 +7,15 @@ def get_compute_device(force_cpu=False, prefer_last=True):
     if CUDA_available:
         CUDA_count = torch.cuda.device_count()
 
-    compute_device = torch.device('cpu')
+    compute_device = torch.device("cpu")
 
     if force_cpu or not CUDA_available:
         return compute_device
 
     elif prefer_last and CUDA_count > 1:
-        compute_device = torch.device(f'cuda:{CUDA_count - 1}')
+        compute_device = torch.device(f"cuda:{CUDA_count - 1}")
         return compute_device
 
     else:
-        compute_device = torch.device('cuda')
-        return torch.device('cuda:0')
+        compute_device = torch.device("cuda")
+        return torch.device("cuda:0")
