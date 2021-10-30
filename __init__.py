bd17b32287ab8f658420fde2a2ad26518560a96a
sgbaird <sterling.baird@icloud.com>
2021-10-30 06:06:13 -0600

    move init

--- __init__.py
+++
@@ -1,2 +0,0 @@
-"""Predict materials properties using only the composition information."""
-__version__ = "1.1.0"
---
+++ crabnet/__init__.py
@@ -0,0 +1,2 @@
+"""Predict materials properties using only the composition information."""
+__version__ = "1.0.0"
