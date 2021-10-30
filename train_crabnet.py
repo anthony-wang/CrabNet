a9cdf6798bd5ad8927514362da9d3bc174e64d31
sgbaird <sterling.baird@icloud.com>
2021-10-30 06:07:03 -0600

    fixup imports

--- train_crabnet.py
+++ train_crabnet.py
@@ -12,10 +12,10 @@ import torch
 
 from sklearn.metrics import roc_auc_score
 
-from mat_discover.CrabNet.crabnet.kingcrab import CrabNet  # type: ignore
-from mat_discover.CrabNet.crabnet.model import Model  # type: ignore
+from CrabNet.crabnet.kingcrab import CrabNet  # type: ignore
+from CrabNet.crabnet.model import Model  # type: ignore
 
-from mat_discover.CrabNet.utils.get_compute_device import get_compute_device
+from CrabNet.utils.get_compute_device import get_compute_device
 
 RNG_SEED = 42
 torch.manual_seed(RNG_SEED)
