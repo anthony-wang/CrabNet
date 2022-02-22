8601a0c83469fb9f2bbdcc91568753debd1c44fa
sgbaird <sterling.baird@icloud.com>
2022-02-22 00:49:08 -0700

    Update crabnet_basic.py

--- crabnet/examples/crabnet_basic.py
+++ crabnet/examples/crabnet_basic.py
@@ -1,6 +1,7 @@
-"""Basic usage of CrabNet regression on elasticity dataset."
+"""Basic usage of CrabNet regression on elasticity dataset."""
 from crabnet.model import get_data
 from crabnet.data.materials_data import elasticity
+
 train_df, val_df = get_data(elasticity, "train.csv", dummy=True)
 
 from crabnet.crabnet_ import CrabNet
