1ac8a3c0e6c21e3cb3400067642d8f07b696b673
Sterling Baird <45469701+sgbaird@users.noreply.github.com>
2022-02-22 00:28:02 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""This file exists to set up the Github "Used By" counter properly and is not meant to be run."""
+from setuptools import setup
+
+
+raise Exception(
+    """
+    This file exists to set up the Github "Used By" counter properly and is not meant to be run.
+    """
+)
+
+setup(name="crabnet")
