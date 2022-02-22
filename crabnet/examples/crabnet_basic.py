63c35eccbf2d91945de1a15591d3a0dfaafa1bf7
Sterling Baird <45469701+sgbaird@users.noreply.github.com>
2022-02-22 00:43:44 -0700

    Create crabnet_basic.py

---
+++ crabnet/examples/crabnet_basic.py
@@ -0,0 +1,10 @@
+"""Basic usage of CrabNet regression on elasticity dataset."
+from crabnet.model import get_data
+from crabnet.data.materials_data import elasticity
+train_df, val_df = get_data(elasticity, "train.csv", dummy=True)
+
+from crabnet.crabnet_ import CrabNet
+
+cb = CrabNet(mat_prop="elasticity")
+cb.fit(train_df)
+val_pred, val_sigma = cb.predict(val_df)
