922cede78c689d20e30774d42148cb61d53ed4a1
sgbaird <sterling.baird@icloud.com>
2022-02-21 22:49:33 -0700

    rename _CrabNet to Subcrab, and documentation

--- crabnet/kingcrab.py
+++ crabnet/kingcrab.py
@@ -1,6 +1,5 @@
-#%%
+"""Contains classes for transformer architecture within CrabNet."""
 from os.path import join, dirname
-from turtle import forward
 
 import numpy as np
 import pandas as pd
@@ -9,7 +8,6 @@ import torch
 from torch import nn
 from collections import OrderedDict
 
-# %%
 RNG_SEED = 42
 torch.manual_seed(RNG_SEED)
 np.random.seed(RNG_SEED)
@@ -20,22 +18,24 @@ data_type_torch = torch.float32
 class ResidualNetwork(nn.Module):
     """
     Feed forward Residual Neural Network as seen in Roost.
+    
     https://doi.org/10.1038/s41467-020-19964-7
-
-    Parameters
-    ----------
-    input_dim : int
-        Input dimensions for the Residual Network, specified in CrabNet() model class, by default 512
-    output_dim : int
-        Output dimensions for Residual Network, by default 3
-    hidden_layer_dims : list(int)
-        Hidden layer architecture for the Residual Network, by default [1024, 512, 256, 128]
-    bias : bool
-        Whether to bias the linear network, by default False
-
     """
 
     def __init__(self, input_dim, output_dim, hidden_layer_dims, bias):
+        """Instantiate a ResidualNetwork model.
+        
+        Parameters
+        ----------
+        input_dim : int
+            Input dimensions for the Residual Network, specified in CrabNet() model class, by default 512
+        output_dim : int
+            Output dimensions for Residual Network, by default 3
+        hidden_layer_dims : list(int)
+            Hidden layer architecture for the Residual Network, by default [1024, 512, 256, 128]
+        bias : bool
+            Whether to bias the linear network, by default False
+        """
         super(ResidualNetwork, self).__init__()
         dims = [input_dim] + hidden_layer_dims
         self.fcs = nn.ModuleList(
@@ -53,8 +53,7 @@ class ResidualNetwork(nn.Module):
         self.fc_out = nn.Linear(dims[-1], output_dim)
 
     def forward(self, fea):
-        """
-        Forward method for the Residual Network
+        """Propagate Residual Network weights forward.
 
         Parameters
         ----------
@@ -70,25 +69,28 @@ class ResidualNetwork(nn.Module):
         return self.fc_out(fea)
 
     def __repr__(self):
+        """Return the class name."""
         return f"{self.__class__.__name__}"
 
 
 class TransferNetwork(nn.Module):
-    """
-    Model class for learning extended representations of materials during transfer
-    learning. This network was designed to have little impact on predictions during
+    """Learn extended representations of materials during transfer learning.
+    
+    This network was designed to have little impact on predictions during
     training and enhance learning with the inclusion of extended features.
-
-    Parameters
-    ----------
-    input_dims : int
-        Dimensions of input layer
-
-    output_dims : int
-        Dimensions of ouput layer
     """
 
     def __init__(self, input_dims, output_dims):
+        """Instantiate a TransferNetwork to learn extended representations.
+        
+        Parameters
+        ----------
+        input_dims : int
+            Dimensions of input layer
+
+        output_dims : int
+            Dimensions of output layer
+        """
         super().__init__()
         self.layers = nn.Sequential(
             OrderedDict(
@@ -102,29 +104,41 @@ class TransferNetwork(nn.Module):
         )
 
     def forward(self, x):
+        """Perform a forward pass of the TransferNetwork.
+
+        Parameters
+        ----------
+        x : _type_
+            _description_
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
         x = self.layers(x)
         return x
 
 
 class Embedder(nn.Module):
-    """
-    Embedding class performs mat2vec embeddings of elemental features
-
-    Parameters
-    ----------
-    d_model : int
-        Row dimenions of elemental emeddings, by default 512
-    compute_device : str
-        Name of device which the model will be run on
-    elem_prop : str
-        Which elemental feature vector to use. Possible values are "jarvis", "magpie",
-        "mat2vec", "oliynyk", "onehot", "ptable", and "random_200", by default "mat2vec"
-
-    """
+    """Perform composition-based embeddings of elemental features."""
 
     def __init__(
         self, d_model: int, compute_device: str = None, elem_prop: str = "mat2vec",
     ):
+        """Embed elemental features, similar to CBFV.
+        
+        Parameters
+        ----------
+        d_model : int
+            Row dimenions of elemental emeddings, by default 512
+        compute_device : str
+            Name of device which the model will be run on
+        elem_prop : str
+            Which elemental feature vector to use. Possible values are "jarvis",
+            "magpie", "mat2vec", "oliynyk", "onehot", "ptable", and "random_200", by
+            default "mat2vec"
+        """
         super().__init__()
         self.d_model = d_model
         self.compute_device = compute_device
@@ -146,16 +160,13 @@ class Embedder(nn.Module):
             self.compute_device, dtype=data_type_torch
         )
 
-    def forward(self, src, extra_features=None):
-        """
-        Forward call for embedder class, performs elemental embeddings
+    def forward(self, src):
+        """Compute forward call for embedder class to perform elemental embeddings.
 
         Parameters
         ----------
         src : torch.tensor
             Tensor containing element numbers corresponding to elements in compound
-        extra_features : torch.tensor, optional
-            Tensor containing features not captured by composition, by default None
 
         Returns
         -------
@@ -169,25 +180,26 @@ class Embedder(nn.Module):
 
 # %%
 class FractionalEncoder(nn.Module):
-    """
-    Encoding element fractional amount using a "fractional encoding" inspired
-    by the positional encoder discussed by Vaswani.
+    """Encode element fractional amount using a "fractional encoding".
+    
+    This is inspired by the positional encoder discussed by Vaswani.
     https://arxiv.org/abs/1706.03762
-
-    Parameters
-    ----------
-    d_model : int
-        Model size, see paper, by default 512
-    resolution : int
-        Number of discretizations for the fractional prevalence encoding, by default 100
-    log10 : bool
-        Whether to apply a log operation to fraction prevalence encoding, by default False
-    compute_device : str
-        The compute device to store and run the FractionalEncoder class
-
     """
 
     def __init__(self, d_model, resolution=100, log10=False, compute_device=None):
+        """Instantiate the FractionalEncoder.
+        
+        Parameters
+        ----------
+        d_model : int
+            Model size, see paper, by default 512
+        resolution : int
+            Number of discretizations for the fractional prevalence encoding, by default 100
+        log10 : bool
+            Whether to apply a log operation to fraction prevalence encoding, by default False
+        compute_device : str
+            The compute device to store and run the FractionalEncoder class
+        """
         super().__init__()
         self.d_model = d_model // 2
         self.resolution = resolution
@@ -209,8 +221,7 @@ class FractionalEncoder(nn.Module):
         pe = self.register_buffer("pe", pe)
 
     def forward(self, x):
-        """
-        Forward method for the FractionalEncoder class, performs fractional encoding
+        """Perform the forward pass of the fractional encoding.
 
         Parameters
         ----------
@@ -236,41 +247,10 @@ class FractionalEncoder(nn.Module):
 
 # %%
 class Encoder(nn.Module):
-    """
-    Ecoder class that performs and merges elemental embeddings and fractional encodings
-    to form elemental descriptor matrix, see paper.
-
-    Parameters
-    ----------
-    d_model : _type_
-        _description_
-    N : int, optional
-        Number of encoder layers, by default 3
-    heads : int, optional
-        Number of attention heads to use, by default 4
-    extend_features : bool, optional
-        Specify whether extended features are being used, by default None
-    frac : bool, optional
-        Tensor containing fractional amounts of each element in compound, by default False
-    attn : bool, optional
-        Whether to perform self attention, by default True
-    pe_resolution : int, optional
-        Number of discretizations for the prevalence encoding, by default 5000
-    ple_resolution : int, optional
-        Number of discretizations for the prevalence log encoding, by default 5000
-    elem_prop : str, optional
-        Which elemental feature vector to use. Possible values are "jarvis", "magpie",
-        "mat2vec", "oliynyk", "onehot", "ptable", and "random_200", by default "mat2vec"
-    emb_scaler : float, optional
-        _description_, by default 1.0
-    pos_scaler : float, optional
-        Scaling factor applied to fractional encoder, by default 1.0
-    pos_scaler_log : float, optional
-        Scaling factor applied to log fractional encoder, by default 1.0
-    dim_feedforward : int, optional
-        Dimensions of the feed forward network appied after attention mechanism, by default 2048
-    dropout : float, optional
-        Percent dropout for feedforward attention heads, by default 0.1
+    """Create elemental descriptor matrix via element embeddings and frac. encodings.
+    
+    See the CrabNet paper for further details:
+    https://www.nature.com/articles/s41524-021-00545-1
     """
 
     def __init__(
@@ -291,6 +271,40 @@ class Encoder(nn.Module):
         dim_feedforward=2048,
         dropout=0.1,
     ):
+        """Instantiate the Encoder class to create elemental descriptor matrix (EDM).
+        
+        Parameters
+        ----------
+        d_model : _type_
+            _description_
+        N : int, optional
+            Number of encoder layers, by default 3
+        heads : int, optional
+            Number of attention heads to use, by default 4
+        extend_features : bool, optional
+            Specify whether extended features are being used, by default None
+        frac : bool, optional
+            Tensor containing fractional amounts of each element in compound, by default False
+        attn : bool, optional
+            Whether to perform self attention, by default True
+        pe_resolution : int, optional
+            Number of discretizations for the prevalence encoding, by default 5000
+        ple_resolution : int, optional
+            Number of discretizations for the prevalence log encoding, by default 5000
+        elem_prop : str, optional
+            Which elemental feature vector to use. Possible values are "jarvis", "magpie",
+            "mat2vec", "oliynyk", "onehot", "ptable", and "random_200", by default "mat2vec"
+        emb_scaler : float, optional
+            _description_, by default 1.0
+        pos_scaler : float, optional
+            Scaling factor applied to fractional encoder, by default 1.0
+        pos_scaler_log : float, optional
+            Scaling factor applied to log fractional encoder, by default 1.0
+        dim_feedforward : int, optional
+            Dimensions of the feed forward network appied after attention mechanism, by default 2048
+        dropout : float, optional
+            Percent dropout for feedforward attention heads, by default 0.1
+        """
         super().__init__()
         self.d_model = d_model
         self.N = N
@@ -328,8 +342,7 @@ class Encoder(nn.Module):
             )
 
     def forward(self, src, frac, extra_features=None):
-        """
-        Forward method for Encoder class
+        """Compute the forward pass for encoding the elemental descriptor matrix.
 
         Parameters
         ----------
@@ -384,49 +397,8 @@ class Encoder(nn.Module):
 
 
 # %%
-class _CrabNet(nn.Module):
-    """
-    CrabNet model class
-
-    Parameters
-    ----------
-    out_dims : int, optional
-        Output dimensions for Residual Network, by default 3
-    d_model : int, optional
-        Model size. See paper, by default 512
-    extend_features : _type_, optional
-        Additional features to grab from columns of the other DataFrames (e.g. state
-        variables such as temperature or applied load), by default None
-    d_extend : int, optional
-        Number of extended features, by default 0
-    N : int, optional
-        Number of attention layers, by default 3
-    heads : int, optional
-        Number of attention heads, by default 4
-    compute_device : _type_, optional
-        Computing device to run model on, by default None
-    out_hidden : list(int), optional
-        Architecture of hidden layers in the Residual Network, by default [1024, 512, 256, 128]
-    pe_resolution : int, optional
-        Number of discretizations for the prevalence encoding, by default 5000
-    ple_resolution : int, optional
-        Number of discretizations for the prevalence log encoding, by default 5000
-    elem_prop : str, optional
-        Which elemental feature vector to use. Possible values are "jarvis", "magpie",
-        "mat2vec", "oliynyk", "onehot", "ptable", and "random_200", by default "mat2vec"
-    bias : bool, optional
-        Whether to bias the Residual Network, by default False
-    emb_scaler : float, optional
-        Float value by which to scale the elemental embeddings, by default 1.0
-    pos_scaler : float, optional
-        Float value by which to scale the fractional encodings, by default 1.0
-    pos_scaler_log : float, optional
-        Float value by which to scale the log fractional encodings, by default 1.0
-    dim_feedforward : int, optional
-        Dimenions of the feed forward network following transformer, by default 2048
-    dropout : float, optional
-        Percent dropout in the feed forward network following the transformer, by default 0.1
-    """
+class SubCrab(nn.Module):
+    """SubCrab model class which implements the transformer architecture."""
 
     def __init__(
         self,
@@ -448,7 +420,47 @@ class _CrabNet(nn.Module):
         dim_feedforward=2048,
         dropout=0.1,
     ):
-
+        """Instantiate a SubCrab class to be used within CrabNet.
+        
+        Parameters
+        ----------
+        out_dims : int, optional
+            Output dimensions for Residual Network, by default 3
+        d_model : int, optional
+            Model size. See paper, by default 512
+        extend_features : _type_, optional
+            Additional features to grab from columns of the other DataFrames (e.g. state
+            variables such as temperature or applied load), by default None
+        d_extend : int, optional
+            Number of extended features, by default 0
+        N : int, optional
+            Number of attention layers, by default 3
+        heads : int, optional
+            Number of attention heads, by default 4
+        compute_device : _type_, optional
+            Computing device to run model on, by default None
+        out_hidden : list(int), optional
+            Architecture of hidden layers in the Residual Network, by default [1024, 512, 256, 128]
+        pe_resolution : int, optional
+            Number of discretizations for the prevalence encoding, by default 5000
+        ple_resolution : int, optional
+            Number of discretizations for the prevalence log encoding, by default 5000
+        elem_prop : str, optional
+            Which elemental feature vector to use. Possible values are "jarvis", "magpie",
+            "mat2vec", "oliynyk", "onehot", "ptable", and "random_200", by default "mat2vec"
+        bias : bool, optional
+            Whether to bias the Residual Network, by default False
+        emb_scaler : float, optional
+            Float value by which to scale the elemental embeddings, by default 1.0
+        pos_scaler : float, optional
+            Float value by which to scale the fractional encodings, by default 1.0
+        pos_scaler_log : float, optional
+            Float value by which to scale the log fractional encodings, by default 1.0
+        dim_feedforward : int, optional
+            Dimenions of the feed forward network following transformer, by default 2048
+        dropout : float, optional
+            Percent dropout in the feed forward network following the transformer, by default 0.1
+        """
         super().__init__()
         self.avg = True
         self.out_dims = out_dims
@@ -483,8 +495,7 @@ class _CrabNet(nn.Module):
         )
 
     def forward(self, src, frac, extra_features=None):
-        """
-        Forward method of the CrabNet model class
+        """Compute forward pass of the SubCrab model class (i.e. transformer).
 
         Parameters
         ----------
@@ -501,9 +512,8 @@ class _CrabNet(nn.Module):
             Model output containing predicted value and undcertainty for that value
         """
         output = self.encoder(src, frac, extra_features)
-        # print('WE GOT THIS FAR')
         # output = self.transfer_nn(output)
-        
+
         # average the "element contribution" at the end
         # mask so you only average "elements"
         mask = (src == 0).unsqueeze(-1).repeat(1, 1, self.out_dims)
@@ -521,4 +531,4 @@ class _CrabNet(nn.Module):
 
 # %%
 if __name__ == "__main__":
-    model = _CrabNet()
+    model = SubCrab()
